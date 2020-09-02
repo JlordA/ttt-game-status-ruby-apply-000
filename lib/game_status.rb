f51c87944437d0a4b5c4b51b600ca329490daec7
placid-tier-4550 <james.ardery@gmail.com>
2020-09-02 12:38:28 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,8 +36,9 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) || draw?(board) || full?(board)  
+  if won?(board) || draw?(board) || full?(board)
     true
   else
     false
+  end
 end
